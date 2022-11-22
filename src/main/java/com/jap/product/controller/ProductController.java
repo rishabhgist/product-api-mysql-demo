79045facd5d86c754233ce8f4d8ce3783cfdcdfb
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:22:03 +0530

    added ProductService object and initialized using the constructor

--- src/main/java/com/jap/product/controller/ProductController.java
+++ src/main/java/com/jap/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.jap.product.controller;
 
+import com.jap.product.service.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,4 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1")
 public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 }
