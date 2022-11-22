28898fcb046fa4616d51f07b59c3b1c9887e0c0a
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:09:32 +0530

    added abstract method findById

--- src/main/java/com/jap/product/repository/ProductRepository.java
+++ src/main/java/com/jap/product/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.jap.product.repository;
 
-public interface ProductRepository {
+import com.jap.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    List<Product> findById(Integer id);
 }
