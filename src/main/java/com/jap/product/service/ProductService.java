92bafa50a968aa8409997df35d0a7b73694a51a1
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:18:01 +0530

    added autowired for the constructor

--- src/main/java/com/jap/product/service/ProductService.java
+++ src/main/java/com/jap/product/service/ProductService.java
@@ -1,12 +1,13 @@
 package com.jap.product.service;
 
 import com.jap.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
     private ProductRepository productRepository;
-
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
