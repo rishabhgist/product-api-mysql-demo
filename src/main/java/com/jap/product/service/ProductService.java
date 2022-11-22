930e89a2ea4ffe2f4eddcd140797feda8a042ff1
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:17:20 +0530

    added ProductRepository object and initialized using constructor

--- src/main/java/com/jap/product/service/ProductService.java
+++ src/main/java/com/jap/product/service/ProductService.java
@@ -1,8 +1,13 @@
 package com.jap.product.service;
 
+import com.jap.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    private ProductRepository productRepository;
 
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
