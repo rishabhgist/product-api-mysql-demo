fdfe71d7febc122a7b3996b0d217c5f6ee6e84cf
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:01:57 +0530

    added no args and parameterized constructor

--- src/main/java/com/jap/product/domain/Product.java
+++ src/main/java/com/jap/product/domain/Product.java
@@ -5,4 +5,16 @@ public class Product {
     private String name;
     private String description;
     private double amount;
+
+    public Product(int id, String name, String description, double amount) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Product() {
+    }
+
+
 }
