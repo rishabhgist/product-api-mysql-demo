136afcffdabc555ba18133dab4971ad30c92105a
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:02:17 +0530

    added getters and setters

--- src/main/java/com/jap/product/domain/Product.java
+++ src/main/java/com/jap/product/domain/Product.java
@@ -16,5 +16,35 @@ public class Product {
     public Product() {
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
