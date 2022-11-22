8844f6731f2b4ac76708a436e1272c785ecad2a2
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:15:30 +0530

    added abstract methods in ProductService interface

--- src/main/java/com/jap/product/service/IProductService.java
+++ src/main/java/com/jap/product/service/IProductService.java
@@ -1,4 +1,11 @@
 package com.jap.product.service;
 
+import com.jap.product.domain.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    Product saveProduct(Product product);
+    List<Product> getAllProduct();
+    List<Product> getProductByName(String name);
 }
